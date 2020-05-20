9a29ae31c25703169aec35192fef2499843a1f13
Nahila Zaiba <nahelazaiba@gmail.com>
2020-05-20 15:51:53 -0400

    Added file contents

---
+++ payment.java
@@ -0,0 +1,12 @@
+Payment details will be entered here.
+
+public Payment{
+
+	public static void main(String args[]) {
+		float dollar;
+		system.out.println("Enter the required amount");
+		Sacnner sc = new Sacnner(System.in);
+		dollar = sc.Interger.ParseInt();
+		system.out.println("Amount Entered:" +dollar);
+	}
+}
